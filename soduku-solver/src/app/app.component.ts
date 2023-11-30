28dd417217272feb6aaceac5e294b68d9ceaf3ab
Aymen Nacer <aymen@nacer.com>
2023-11-30 16:33:05 +0300

    Use drawboard service to draw a board in app component

--- soduku-solver/src/app/app.component.ts
+++ soduku-solver/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { DrawBoardService } from './draw-board.service';
+import { LoadBoardsService } from './load-boards.service';
+import { SolverService } from './solver.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'soduku-solver';
+  @ViewChild('puzzle') puzzleElement!: ElementRef;
+
+  constructor(
+    private drawBoard: DrawBoardService,
+    private loadBoards: LoadBoardsService,
+    private solver: SolverService
+  ) {}
+
+  ngAfterViewInit() {
+    const sudokuBoard: Element = this.puzzleElement.nativeElement;
+    this.drawBoard.drawBoard(sudokuBoard);
+  }
+
+  ngOnInit() {}
+
+  onSolved() {
+    console.log('onSolved');
+  }
+
+  onCleared() {
+    console.log('onCleared');
+  }
+
+  onLoaded() {
+    console.log('onLoaded');
+  }
 }
--- soduku-solver/src/app/draw-board.service.ts
+++ soduku-solver/src/app/draw-board.service.ts
@@ -1,34 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DrawBoardService {
+  private renderer: Renderer2;
 
-  
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
 
-  constructor() { }
+  drawBoard(sudokuBoard: Element) {
+    const squares = 81;
 
-
-   drawBoard(sudokuBoard: Element) {
-    const squares = 81
-
-    for (let i=0; i<squares; i++) {
-        const inputElement = document.createElement("input")
-        inputElement.setAttribute('type', 'number')
-        inputElement.setAttribute('min', '1')
-        inputElement.setAttribute('max', '9')
-
-        if (
-            ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
-            ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
-            ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && (i > 27 && i < 53)) ||
-            ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
-            ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
-        ) {
-            inputElement.classList.add('odd-section')
-        }
-        sudokuBoard.appendChild(inputElement)
+    for (let i = 0; i < squares; i++) {
+      const inputElement = this.renderer.createElement('input');
+      this.renderer.setAttribute(inputElement, 'type', 'number');
+      this.renderer.setAttribute(inputElement, 'min', '1');
+      this.renderer.setAttribute(inputElement, 'max', '9');
+      console.log(i);
+      if (
+        ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+        ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+        ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
+        ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+        ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+      ) {
+        this.renderer.addClass(inputElement, 'odd-section');
+      }
+      this.renderer.appendChild(sudokuBoard, inputElement);
     }
-}
+  }
 }
